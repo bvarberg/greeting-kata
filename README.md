5b48eea95c6261314319238b1bfee9e80f80f83f
Byron Varberg <byron@crema.us>
2020-08-07 15:55:19 -0500

    Add skeleton for the exercise

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # greeting-kata
+
+See [testdouble's `contributing-tests` wiki](https://github.com/testdouble/contributing-tests/wiki/Greeting-Kata) for the actual exercise and a ton of useful resources around testing + TDD.
---
+++ src/greet.test.ts
@@ -0,0 +1,8 @@
+describe('greet', () => {
+  /**
+   * Requirement 1:
+   * Write a method greet(name) that interpolates name in a simple greeting.
+   * For example, when name is "Bob", the method should return a string "Hello, Bob."
+   */
+  test.todo('a simple greeting')
+})
